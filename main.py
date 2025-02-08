f67edc8c49f5a6511dd3fcef92142a800d784d75
Soulter <37870767+Soulter@users.noreply.github.com>
2025-02-09 00:07:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from astrbot.api.all import Context
 # 让 astrbot 的依赖安装器检测到
 import cryptography
 import wechatpy
-import pydub
+try
+    import pydub
+except Exception:
+    pass
 
 class Main:
     def __init__(self, context: Context) -> None:
